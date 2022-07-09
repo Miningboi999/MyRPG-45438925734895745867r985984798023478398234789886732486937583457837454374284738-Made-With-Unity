caedd36adba41d6bdd79dba78199192596080b07
Miningboi999 <108203889+Miningboi999@users.noreply.github.com>
2022-07-09 13:48:45 +0530

    .

---
+++ My Game/Assets/Scripts/Stats/PlayerStats.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : CharStats
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        EquipmentManager.instance.onEquipmentChanged += OnEquipmentChanged;
+    }
+
+    void OnEquipmentChanged(Equipment newItem, Equipment oldItem)
+    {
+        
+    }
+}
--- My Game/Assets/Scripts/Stats/Stat.cs
+++ My Game/Assets/Scripts/Stats/Stat.cs
@@ -8,11 +8,16 @@ public class Stat
 
 [SerializeField]
 private int baseValue;
+    private List<int> modifiers = new List<int>();
+
 
 public int GetValue ()
 {
     return baseValue;
 }
+
+
+
 
 
 }
