665c3f870d287f9868d76213c60f0b79837bf450
Miningboi999 <108203889+Miningboi999@users.noreply.github.com>
2022-07-11 09:11:09 +0530

    .

--- My Game/Assets/Scripts/Stats/PlayerStats.cs
+++ My Game/Assets/Scripts/Stats/PlayerStats.cs
@@ -14,6 +14,16 @@ public class PlayerStats : CharStats
 
     void OnEquipmentChanged(Equipment newItem, Equipment oldItem)
     {
+        if (newItem != null)
+        {
+            armor.AddModifier(newItem.armorModifier);
+            damage.AddModifier(newItem.damageModifier);
+        }
 
+        if (oldItem != null)
+        {
+            armor.RemoveModifier(oldItem.armorModifier);
+            damage.RemoveModifier(oldItem.damageModifier);
+        }
     }
 }
--- My Game/Assets/Scripts/Stats/Stat.cs
+++ My Game/Assets/Scripts/Stats/Stat.cs
@@ -6,20 +6,29 @@ using UnityEngine;
 public class Stat
 {
 
-[SerializeField]
-private int baseValue;
+    [SerializeField]
+    private int baseValue;
     private List<int> modifiers = new List<int>();
 
 
-public int GetValue ()
-{
-    return baseValue;
-}
+    public int GetValue()
+    {
+        int finalValue = baseValue;
+        modifiers.ForEach(x => finalValue += x);
+        return finalValue;
+    }
 
-public void AddModifier (int modifier)
-{
-    
-}
+    public void AddModifier(int modifier)
+    {
+        if (modifier != 0)
+            modifiers.Add(modifier);
+    }
+
+    public void RemoveModifier(int modifier)
+    {
+        if (modifier != 0)
+            modifiers.Remove(modifier);
+    }
 
 
 
