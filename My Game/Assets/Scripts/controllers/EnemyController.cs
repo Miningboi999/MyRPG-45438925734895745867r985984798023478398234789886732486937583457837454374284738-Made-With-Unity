18974d46e707b3fc93647d4092b45ce812cf8238
Miningboi999 <108203889+Miningboi999@users.noreply.github.com>
2022-07-12 19:18:36 +0530

    .

--- My Game/Assets/Scripts/controllers/EnemyController.cs
+++ My Game/Assets/Scripts/controllers/EnemyController.cs
@@ -5,10 +5,10 @@ using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
-public float lookRadius = 10f;
+    public float lookRadius = 10f;
 
-Transform target;
-NavMeshAgent agent;
+    Transform target;
+    NavMeshAgent agent;
 
     // Start is called before the first frame update
     void Start()
@@ -21,10 +21,26 @@ NavMeshAgent agent;
     void Update()
     {
         float distance = Vector3.Distance(target.position, transform.position);
-        #pragma warning restore format
+
+        if (distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+            if (distance <= agent.stoppingDistance)
+            {
+                FaceTarget();
+            }
+        }
     }
 
-    void OnDrawGizmosSelected ()
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime) ;
+    }
+
+
+    void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, lookRadius);
